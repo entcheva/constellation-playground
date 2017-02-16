929c39828d14f770b85836421c42d5262a8c5b9b
entcheva <christina.entcheva@gmail.com>
2017-02-16 14:19:43 -0500

    Add basic canvas

---
+++ src/components/Canvas.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+export default class Canvas extends Component {
+
+  componentDidMount() {
+    const canvas = this.refs.starfield
+    const context = canvas.getContext('2d')
+    const stars = 200
+    // context.fillRect(10, 10, 10, 10)
+    // context.fillStyle = "black"
+    for (let i = 0; i < stars; i++) {
+      let x = Math.random() * canvas.offsetWidth
+      let y = Math.random() * canvas.offsetHeight
+      let radius = Math.random() * 1.2
+      context.beginPath()
+      context.arc(x, y, radius, 0, 360)
+      context.fillStyle = "hsla(200, 100%, 50%, 0.8)"
+      context.fill()
+    }
+
+  }
+
+
+
+
+
+  render () {
+    return (
+      <div>
+        <canvas ref="starfield" width={750} height={500}></canvas>
+      </div>
+    )
+  }
+
+}
--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ import { Route, IndexRoute } from 'react-router'
 import App from './components/App'
 import User from './components/User'
 import Sky from './components/Sky'
+import Canvas from './components/Canvas'
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={Sky} />
     <Route path="/signup" component={User}></Route>
+    <Route path="/canvas" component={Canvas}></Route>
   </Route>
 )
